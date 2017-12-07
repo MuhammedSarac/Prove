ce1b039b8e274e54f3a597235f70272cae25a4ff
MuhammedSarac <muhammedsarac@live.dk>
2017-12-07 12:23:03 +0100

    Add project files.

---
+++ TorsdagsWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorsdagsWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Adition(int tal1, int tal2)
+        {
+            return tal1 + tal2;
+        }
+
+       
+
+    }
+}
